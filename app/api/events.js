f0ab5fcb212f43fdb907b6eff355a0a8b47c62b8
AndyScorp <scorpion.and@ukr.net>
2018-05-20 00:14:51 +0300

    add filter to events route

--- app/api/events.js
+++ app/api/events.js
@@ -10,7 +10,29 @@ events.get('/all', (req, res) => {
                     name: event.name,
                     place: {
                         lat: event.place.lat,
-                        tng: event.place.tng
+                        lng: event.place.lng
+                    }
+                }
+            });
+            res.json([ ...events ]);
+        })
+        .catch(error => res.status(500).json({ error }));
+});
+
+// nned to test
+events.get('/filter', (req, res) => {
+    const query = Object.assign({}, req.query);
+    Event.find({
+        sport_type:  { $in: query.sport_type }
+    })
+        .then(events => {
+            events = events.map(function(event) {
+                return {
+                    id: event._id,
+                    name: event.name,
+                    place: {
+                        lat: event.place.lat,
+                        lng: event.place.lng
                     }
                 }
             });
