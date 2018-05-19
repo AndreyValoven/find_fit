0c8358fc5646615461fe7f5b96f158e2a0396ae2
AndyScorp <scorpion.and@ukr.net>
2018-05-19 19:19:41 +0300

    fix user and index

--- app/api/index.js
+++ app/api/index.js
@@ -2,5 +2,9 @@ const api = require('express').Router();
 
 api.use('/user', require('./user'));
 
+api.use('/login', require('./login'));
+api.use('/registration', require('./registration'));
+api.use('/event', require('./event'));
+api.use('/events', require('./events'));
 
 module.exports = api;
--- app/api/user.js
+++ app/api/user.js
@@ -1,75 +1,27 @@
 const user = require('express').Router();
-const Ajv = require('ajv');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
 
+const validteToken = require('./../validateToken');
 const User = require('./../schemas/user');
 
-let ajv = new Ajv({ allErrors: true });
 
-const registrationSchema = {
-    type: 'object',
-    additionaProperties: false,
-    properties: {
-        name: { type: 'string' , minLength: 1},
-        sport_type: { type: 'string' , minLength: 1},
-        number: { type: 'number' },
-        link: { type: 'string' }
-    },
-    required: [ 'name' ]
-}
-
-const registrationValidate = ajv.compile(registrationSchema);
-
-user.post('/registration', 
-    (req, res, next) => {
-        if (!registrationValidate(req.body)) return res.status(500).json({ error: registrationValidate.errors });
-        next();
-    },
-    (req, res) => {
-        let body = req.body;
-        let user = new User({
-            _id: mongoose.Types.ObjectId(),
-            name: req.body.name,
-            contacts: {
-                number: body.number,
-                link: body.link
-            }
+user.get('/:id', (req, res) => {
+    User.findOne( { _id: req.params.id})
+        .then(user => {
+            res.json({ user });
         })
+});
 
-        user.save()
-            .then(user => {
-                let token = jwt.sign( { id: user._id , name: user.name }, 'shh' );
-                res.json({ 
-                    token,
-                    name: user.name,
-                    id: user._id
-                });
-            })
-            .catch(error => res.status(500).json({ error }));
+user.delete('/:id', validteToken, (req, res) => {
+    if (req.id + '' !== req.params.id+ '') {
+        return res.status(403).json({ erorr: 'Forbidden'});
+    }
+    User.findByIdAndRemove(req.id ,(error, user) => {
+        if(error)
+            return res.json({ error });
+        res.json({
+            user
+        });
     });
-
-user.post('/login', 
-    (req, res, next) => {
-        if (!registrationValidate(req.body)) return res.status(500).json({ error: registrationValidate.errors });
-        next();
-    },
-    (req, res) => {
-        let body = req.body;
-        User.findOne({ name: body.name })
-            .then(user => {
-                let token = jwt.sign( { id: user._id , name: user.name }, 'shh' );
-                res.json({ 
-                    token,
-                    name: user.name,
-                    id: user._id
-                });
-            })
-            .catch(error => res.status(500).json({ error }));
-            
-    });
-
-
-    
+});
 
 module.exports = user;
