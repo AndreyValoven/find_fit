fdf6d81b8b25bb36b5c64b51614525c7e52c2bf9
AndyScorp <scorpion.and@ukr.net>
2018-05-20 03:21:43 +0300

    fix events filter

--- app/api/events.js
+++ app/api/events.js
@@ -22,21 +22,25 @@ events.get('/all', (req, res) => {
 // nned to test
 events.get('/filter', (req, res) => {
     const query = Object.assign({}, req.query);
+    let startDate = Date.UTC(...query.start_date.split('/'));
+    let endDate = Date.UTC(...query.end_date.split('/'));
     Event.find({
         sport_type:  { $in: query.sport_type }
     })
         .then(events => {
-            events = events.map(function(event) {
-                return {
-                    id: event._id,
-                    name: event.name,
-                    place: {
-                        lat: event.place.lat,
-                        lng: event.place.lng
-                    }
-                }
+            let eventsFormate = [];
+            events.map(function(event) {
+                if (event.date <= endDate && event.date >= startDate)
+                    eventsFormate.push({
+                        id: event._id,
+                        name: event.name,
+                        place: {
+                            lat: event.place.lat,
+                            lng: event.place.lng
+                        }
+                    });
             });
-            res.json([ ...events ]);
+            res.json([ ...eventsFormate ]);
         })
         .catch(error => res.status(500).json({ error }));
 });
