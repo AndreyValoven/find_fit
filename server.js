67551fd313867addb4fb00d750a0123b705b222a
Andrey <scorpion.and@ukr.net>
2021-11-23 22:01:26 +0200

    rewrite init project

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/find_fit');
-
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-const PORT = process.env.PORT || 3000;
-const db = mongoose.connection;
 app.use(express.static('public'));
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log('Connected to mongo');
-});
 
-app.use('/api', require('./app/api/index'));
+// app.use('/api', require('./app/api/index'));
 
 app.listen(PORT, () => {
     console.log('server start http://localhost:' + PORT);
